f8678f51a4407c4384823db24991cbb56dbbc9bd
NNagato <darkdragon236@gmail.com>
2016-07-25 15:47:33 +0700

    complete login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  before_save{email.downcase!}
+  before_save :email_downcase
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_many :lessons
@@ -18,4 +19,34 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: Settings.minimum_password}, allow_nil: true
 
   has_secure_password
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def email_downcase
+    self.email = email.downcase
+  end
 end
