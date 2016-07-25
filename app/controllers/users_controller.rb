abdca555ee78b104b060871cabd4fc1f27b3db4c
tuanhnt1712 <tuanhnt1712@gmail.com>
2016-07-25 15:28:19 +0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,6 +17,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = t("edit.update_profile")
+      redirect_to @user
+    else
+      flash.now[:danger] = t("flash.update_fail")
+      render :edit
+    end
+  end
+
   def show
     @user = User.find_by(id: params[:id])
   end
@@ -23,5 +40,18 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email,
       :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t("flash.danger")
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
