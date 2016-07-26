d89b5cbbd962cdc1baef564976c30fc2c92e11df
NNagato <darkdragon236@gmail.com>
2016-07-26 22:56:12 +0700

    category

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -7,12 +7,17 @@ class WordsController < ApplicationController
     @word = Word.new
   end
 
+  def show
+    @word = Word.find_by(id: params[:id])
+  end
+
   def create
     @word = Word.new(word_params)
     if @word.save
-      flash[:success] = t("flash.success_word")
-      render :index
+      flash[:success] = t("flash.success_created")
+      redirect_to @word
     else
+      flash.now[:danger] = t("flash.create_failed")
       render :new
     end
   end
--- app/models/caterory.rb
+++ app/models/caterory.rb
@@ -1,4 +1,6 @@
-class Caterory < ApplicationRecord
+class Caterory < ActiveRecord::Base
   has_many :lessons
   has_many :words
+  
+  validates :name, presence: true, length: {maximum: Settings.maximum_name_category}
 end
