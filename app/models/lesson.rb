53395b8e74c9022c113ea873c816f90ee6466126
tuanhnt1712 <tuanhnt1712@gmail.com>
2016-07-30 18:07:12 +0700

    show activity

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,16 @@
 class Lesson < ActiveRecord::Base
-  after_create :update_name
+  after_create :update_name, :create_activity
+  
   has_many :activities
   has_many :results
   
   belongs_to :user
   belongs_to :caterory
+
+  def create_activity
+    Activity.create!(activity_type: self.name,
+      lesson_id: self.id, user_id: self.user.id)
+  end
 
   def update_name
     update_attribute(:name, Settings.paragraph + self.caterory.name)
