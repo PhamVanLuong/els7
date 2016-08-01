0a9e1534495bc1bd49cc987361bb8f6ecff8c38d
tuanhnt1712 <tuanhnt1712@gmail.com>
2016-08-01 11:03:15 +0700

    add answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :word
+  
+  validates :mean, presence: true
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,25 @@
 class Word < ActiveRecord::Base
+  after_create :add_answers
+
   has_many :results
-  has_many :answers
+  has_many :answers, dependent: :destroy
   
   belongs_to :caterory
   
   validates :learning_word, presence: true, length: {maximum: Settings.maximum_word}
   validates :meaning, presence: true, length: {maximum: Settings.maximum_meaning}
+
+  accepts_nested_attributes_for :answers, allow_destroy: true,
+    reject_if: proc {|attributes| attributes[:mean].blank?}
+
+  def add_answers
+    Settings.maximum_ans.times do
+      self.answers.create!(mean: Word.random_answer)
+    end
+    self.answers.sample.update_attributes(mean: self.meaning)
+  end
+
+  def self.random_answer
+    Word.all.sample.meaning
+  end
 end
